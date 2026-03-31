71b56bec9859597fe1a4bc01866260b87b61a961
Alex G Ceseña <agc961014@gmail.com>
2026-03-31 11:14:31 -0700

    got weather working with symbols! 🌤

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const timeSpan = document.querySelector('.time')
 const ampmSpan = document.querySelector('.ampm')
+const weatherDiv = document.querySelector('.weather')
 const day = document.querySelector('.date') //actually the whole date div :) 
 const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const weatherDescriptions = {
+     0: '☀️ Clear',
+    1: '🌤️ Mostly Clear',
+    2: '⛅ Partly Cloudy',
+    3: '☁️ Overcast',
+    45: '🌫️ Foggy',
+    61: '🌧️ Light Rain',
+    63: '🌧️ Rain',
+    80: '🌦️ Showers',
+    95: '⛈️ Thunderstorm'
+}
 
 function updateClock(){
-
-console.log(timeSpan)
-
 const now = new Date()
-console.log(now)
-console.log(now.getHours())
-console.log(now.getMinutes())
-console.log(now.getSeconds())
 
 //clock//
 const minutes = String(now.getMinutes()).padStart(2, '0')
@@ -31,3 +36,13 @@ day.textContent = dayNames[dayIndex] + ' ' + dayNumber + ' ' + monthNames[monthI
 }
 updateClock() //initial run on load
 setInterval(updateClock,1000) //update once a sec ;)
+
+async function fetchweather() {
+    const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=32.6249&longitude=-115.4436&current_weather=true&temperature_unit=fahrenheit')
+    const data = await response.json()
+    const weatherSymbol = data.current_weather.weathercode
+    const weatherTemp = data.current_weather.temperature    
+    weatherDiv.textContent = weatherDescriptions[weatherSymbol] + "  " + Math.round(weatherTemp) + '°F'
+}
+
+fetchweather()
